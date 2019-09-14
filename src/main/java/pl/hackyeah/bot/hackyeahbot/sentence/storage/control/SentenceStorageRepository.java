b71a4f7801c55ec01a79940b6f754a9910c6c4f4
Jakub Kowalski <Jakub.Kowalski@sabre.com>
2019-09-14 13:26:29 +0200

    Clean up repository due to start HackYeah!!!

--- src/main/java/pl/hackyeah/bot/hackyeahbot/sentence/storage/control/SentenceStorageRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.hackyeah.bot.hackyeahbot.sentence.storage.control;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.hackyeah.bot.hackyeahbot.sentence.storage.entity.Sentence;
-
-public interface SentenceStorageRepository extends JpaRepository<Sentence, Long> {
-}
